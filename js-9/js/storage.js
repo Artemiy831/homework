eaee268d2d0f53f499fc556336cfbb6b0557e8f3
Artemiy831 <tenartemij93@gmail.com>
2026-02-01 23:17:37 +1000

    Delete js-9 directory

--- js-9/js/storage.js
+++
@@ -1,7 +0,0 @@
-export const getTodosfromLocalSrorage = () => {
-  return JSON.parse(localStorage.getItem("todos"));
-};
-
-export const sertTodosToLocalStorage = todos => {
-  localStorage.setItem("todos", JSON.stringify(todos));
-};
